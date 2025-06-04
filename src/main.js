1971e8be2d7056e787bba3101e487ccc79df4a82
DavidConnack <47108996+DavidConnack@users.noreply.github.com>
2025-06-04 10:15:58 +0300

    remove cron

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { importActualTransactions, initActual, stopActual } from "./actual.js";
 import { getYesterdaysTransactions } from "./scraper.js";
 import { readFile } from "fs/promises";
-import { CronJob } from "cron";
 
 const config = JSON.parse(
   await readFile(process.env.CONFIG_PATH || "/config/config.json", "utf8"),
 );
-const jobCron = process.env.CRON || "0 7 * * *";
 
 async function processAccountMappers(accountMappers) {
   for (const accountMapper of accountMappers) {
@@ -22,21 +20,10 @@ async function processAccountMappers(accountMappers) {
     );
   }
 }
-
-console.log(
-  "actual-israel will now run an initial import and then will then run according to the set schedule",
+await initActual(
+  config.actualServerUrl,
+  config.actualPassword,
+  config.actualBudgetId,
 );
-const job = CronJob.from({
-  cronTime: jobCron,
-  onTick: async function () {
-    await initActual(
-      config.actualServerUrl,
-      config.actualPassword,
-      config.actualBudgetId,
-    );
-    await processAccountMappers(config.accountMappers).catch(console.error);
-    await stopActual();
-  },
-  start: true,
-  runOnInit: true,
-});
+await processAccountMappers(config.accountMappers).catch(console.error);
+await stopActual();
